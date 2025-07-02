11cc5398b7a7d2f9c36bb383a556a80c6d4c8b8d
Anas Alrefai <101460051+anas98re@users.noreply.github.com>
2025-07-02 12:42:06 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,19 @@
+module.exports = {
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.vue",
+  ],
+  fontFamily: {
+  sans: ['Inter', 'sans-serif'],
+  },
+  colors: {
+  'user-bubble-start': '#F2F6FC',
+  'user-bubble-end': '#FBF6FF',
+  },
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
 
